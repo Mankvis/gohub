de772247cc8ca1a611f8f2f72e63478e8b1035c9
mankvis <57199650@qq.com>
2023-09-25 11:22:02 +0800

    factory 和 faker

---
+++ database/fatcories/user_fatcory.go
@@ -0,0 +1,26 @@
+package fatcories
+
+import (
+	"github.com/bxcodec/faker/v3"
+	"gohub/app/models/user"
+	"gohub/pkg/helpers"
+)
+
+func MakeUsers(times int) []user.User {
+
+	var objs []user.User
+
+	// 设置唯一值
+	faker.SetGenerateUniqueValues(true)
+
+	for i := 0; i < times; i++ {
+		model := user.User{
+			Name:     faker.Username(),
+			Email:    faker.Email(),
+			Phone:    helpers.RandomNumber(11),
+			Password: "$2a$14$c5cf765SLcLouABpx/8sbuLPRyHuH4HcdDfHcawXoh0V5yRgIW3Oq",
+		}
+		objs = append(objs, model)
+	}
+	return objs
+}
